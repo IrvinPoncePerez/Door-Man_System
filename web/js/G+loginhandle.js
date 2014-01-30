25cbc0ae014f91b2b4b21be075b288667c1e8ff1
IrvinPoncePerez <IrvinPoncePerez@gmail.com>
2014-01-30 02:03:09 -0600

    Responsive del Login G+

--- web/js/G+loginhandle.js
+++ web/js/G+loginhandle.js
@@ -2,7 +2,17 @@
        var po = document.createElement('script'); 
        po.type = 'text/javascript'; 
        po.async = true;
-       po.src = 'https://apis.google.com/js/client:plusone.js';
+       po.src = 'https://apis.google.com/js/client:plusone.js?onload=render';
        var s = document.getElementsByTagName('script')[0]; 
        s.parentNode.insertBefore(po, s);
 })();
+
+function render() {
+	gapi.signin.render('customBtn', {
+		//'callback' : 'signinCallback',
+		'clientid' : '383828807826-od8raqqomos3vbkseoph3uec76b7ucd1.apps.googleusercontent.com',
+		'cookiepolicy': 'single_host_origin',
+      	'requestvisibleactions': 'http://schemas.google.com/AddActivity',
+      	'scope': 'https://www.googleapis.com/auth/plus.login'
+	});
+}
